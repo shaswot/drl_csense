7b643879665e99ee1abc3495913280d98382f953
liszt <liszt@h100>
2023-06-18 09:45:08 +0000

    wrapper classes consolidated. added time elapsed funcionality to script

--- lib/env_utils.py
+++ lib/env_utils.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, Dict, Optional, Type, Union
 
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import DummyVecEnv, VecEnv, SubprocVecEnv
-from stable_baselines3.common.atari_wrappers import StickyActionEnv, NoopResetEnv, MaxAndSkipEnv, EpisodicLifeEnv, FireResetEnv, WarpFrame, ClipRewardEnv
+from stable_baselines3.common.atari_wrappers import StickyActionEnv, NoopResetEnv, MaxAndSkipEnv, EpisodicLifeEnv, FireResetEnv, WarpFrame, ClipRewardEnv, AtariWrapper
 from stable_baselines3.common.vec_env import VecFrameStack
 
 from stable_baselines3.common.callbacks import BaseCallback
@@ -298,8 +298,146 @@ def make_atari_env_Compressed_VecFrameStack(env_id: Union[str, Callable[..., gym
     env = VecFrameStack(env, n_stack=4)
 
     return env
+##########################################
+class NoisyFrame(gym.ObservationWrapper[np.ndarray, int, np.ndarray]):
+    def __init__(self, env: gym.Env, noise: float) -> None:
+        super().__init__(env)
+        self.noise = noise
+    def observation(self, frame: np.ndarray) -> np.ndarray:
+        if self.noise == 0.0:
+            frame = frame
+        else:
+            try:
+                0.0 <= self.noise < 1.0
+            except ValueError:
+                print(f"Value of noise has to be in range [0,1). Got noise={noise}")
+            noisemask = self.np_random.choice(a=[False, True], size=frame.shape, p=[self.noise, 1.0-self.noise])
+            frame = noisemask * frame
+        return frame
 
+class AtariWrapper_NoisyFrame(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
+    """
+    Atari 2600 preprocessings
 
+    Specifically:
+
+    * Noop reset: obtain initial state by taking random number of no-ops on reset.
+    * Frame skipping: 4 by default
+    * Max-pooling: most recent two observations
+    * Termination signal when a life is lost.
+    * Resize to a square image: 84x84 by default
+    * Grayscale observation
+    * Clip reward to {-1, 0, 1}
+    * Sticky actions: disabled by default
+    * Noisy image: some random pixels are blacked out
+
+    See https://danieltakeshi.github.io/2016/11/25/frame-skipping-and-preprocessing-for-deep-q-networks-on-atari-2600-games/
+    for a visual explanation.
+
+    .. warning::
+        Use this wrapper only with Atari v4 without frame skip: ``env_id = "*NoFrameskip-v4"``.
+
+    :param env: Environment to wrap
+    :param noop_max: Max number of no-ops
+    :param frame_skip: Frequency at which the agent experiences the game.
+        This correspond to repeating the action ``frame_skip`` times.
+    :param screen_size: Resize Atari frame
+    :param terminal_on_life_loss: If True, then step() returns done=True whenever a life is lost.
+    :param clip_reward: If True (default), the reward is clip to {-1, 0, 1} depending on its sign.
+    :param action_repeat_probability: Probability of repeating the last action
+    """
+
+    def __init__(
+        self,
+        env: gym.Env,
+        noop_max: int = 30,
+        frame_skip: int = 4,
+        screen_size: int = 84,
+        terminal_on_life_loss: bool = True,
+        clip_reward: bool = True,
+        action_repeat_probability: float = 0.0,
+        noise: float = 0.0
+    ) -> None:
+        if action_repeat_probability > 0.0:
+            env = StickyActionEnv(env, action_repeat_probability)
+        if noop_max > 0:
+            env = NoopResetEnv(env, noop_max=noop_max)
+        # frame_skip=1 is the same as no frame-skip (action repeat)
+        if frame_skip > 1:
+            env = MaxAndSkipEnv(env, skip=frame_skip)
+        if terminal_on_life_loss:
+            env = EpisodicLifeEnv(env)
+        if "FIRE" in env.unwrapped.get_action_meanings():  # type: ignore[attr-defined]
+            env = FireResetEnv(env)
+        env = WarpFrame(env, width=screen_size, height=screen_size)
+        env = NoisyFrame(env, noise=noise)
+        if clip_reward:
+            env = ClipRewardEnv(env)
+
+        super().__init__(env)
+
+def make_atari_env_Custom(
+    env_id: Union[str, Callable[..., gym.Env]],
+    n_envs: int = 1,
+    seed: Optional[int] = None,
+    start_index: int = 0,
+    monitor_dir: Optional[str] = None,
+    wrapper_class: Optional[Callable[[gym.Env], gym.Env]] = AtariWrapper,
+    wrapper_kwargs: Optional[Dict[str, Any]] = None,
+    env_kwargs: Optional[Dict[str, Any]] = None,
+    vec_env_cls: Optional[Union[Type[DummyVecEnv], Type[SubprocVecEnv]]] = None,
+    vec_env_kwargs: Optional[Dict[str, Any]] = None,
+    monitor_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> VecEnv:
+    
+    """
+    Create a wrapped, monitored VecEnv for Atari.
+    It is a wrapper around ``make_vec_env`` that includes common preprocessing for Atari games.
+
+    :param env_id: either the env ID, the env class or a callable returning an env
+    :param n_envs: the number of environments you wish to have in parallel
+    :param seed: the initial seed for the random number generator
+    :param start_index: start rank index
+    :param monitor_dir: Path to a folder where the monitor files will be saved.
+        If None, no file will be written, however, the env will still be wrapped
+        in a Monitor wrapper to provide additional information about training.
+    :param wrapper_class: Custom Wrapper Class to implement # defaults to AtariWrapper
+    :param wrapper_kwargs: Optional keyword argument to pass to the ``AtariWrapper_Compressed`` or ``AtariWrapper_NoisyFrame``
+    :param env_kwargs: Optional keyword argument to pass to the env constructor
+    :param vec_env_cls: A custom ``VecEnv`` class constructor. Default: None.
+    :param vec_env_kwargs: Keyword arguments to pass to the ``VecEnv`` class constructor.
+    :param monitor_kwargs: Keyword arguments to pass to the ``Monitor`` class constructor.
+    :return: The wrapped environment
+    """
+    return make_vec_env(
+        env_id,
+        n_envs=n_envs,
+        seed=seed,
+        start_index=start_index,
+        monitor_dir=monitor_dir,
+        wrapper_class=wrapper_class,
+        env_kwargs=env_kwargs,
+        vec_env_cls=vec_env_cls,
+        vec_env_kwargs=vec_env_kwargs,
+        monitor_kwargs=monitor_kwargs,
+        wrapper_kwargs=wrapper_kwargs,
+    )
+
+def make_atari_env_Custom_VecFrameStack(env_id: Union[str, Callable[..., gym.Env]], 
+                                        n_envs: int = 1,
+                                        monitor_dir: Optional[str] = None,
+                                        wrapper_class: Optional[Callable[[gym.Env], gym.Env]] = AtariWrapper,
+                                        seed: int = 0,
+                                        wrapper_kwargs: Optional[Dict[str, Any]] = None):
+    env = make_atari_env_Custom(env_id=env_id,
+                                n_envs=n_envs,
+                                monitor_dir = monitor_dir,
+                                wrapper_class=wrapper_class,
+                                seed=seed,
+                                wrapper_kwargs=wrapper_kwargs)
+    env = VecFrameStack(env, n_stack=4)
+
+    return env
 ##########################################
 class SparseView(gym.ObservationWrapper[np.ndarray, int, np.ndarray]):
     def __init__(self, env: gym.Env, sparsity: float) -> None:
@@ -445,15 +583,6 @@ class MyAtariWrapper_nonflipped(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
             env = ClipRewardEnv(env)
 
         super().__init__(env)
-
-class FrameSampling(gym.ObservationWrapper[np.ndarray, int, np.ndarray]):
-    def __init__(self, env: gym.Env, sample_ratio: float) -> None:
-        super().__init__(env)
-        self.sample_ratio = sample_ratio
-    def observation(self, frame: np.ndarray) -> np.ndarray:
-        mask = self.np_random.choice(a=[False, True], size=frame.shape, p=[self.sample_ratio, 1.0-self.sample_ratio])
-        sampled_frame = mask * frame
-        return sampled_frame
 
 def make_my_atari_env(
     env_id: Union[str, Callable[..., gym.Env]],
