4361a314412033c63c1e2d03d8460fdf3616ad6b
liszt <liszt@h100>
2023-06-22 10:53:15 +0000

    using hadamard for wor wizard

--- lib/env_utils.py
+++ lib/env_utils.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 import scipy
+import cv2
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -299,6 +300,102 @@ def make_atari_env_Compressed_VecFrameStack(env_id: Union[str, Callable[..., gym
 
     return env
 ##########################################
+class NoisyWarp(gym.ObservationWrapper[np.ndarray, int, np.ndarray]):
+    def __init__(self, env: gym.Env, width: int = 84, height: int = 84, noise: float = 0.0) -> None:
+        super().__init__(env)
+        self.width = width
+        self.height = height
+        self.noise = noise
+        assert isinstance(env.observation_space, spaces.Box), f"Expected Box space, got {env.observation_space}"
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=(self.height, self.width, 1),
+            dtype=env.observation_space.dtype,  # type: ignore[arg-type]
+        )
+
+    def observation(self, frame: np.ndarray) -> np.ndarray:
+        """
+        returns the current observation from a frame
+
+        :param frame: environment frame
+        :return: the observation
+        """
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        if self.noise == 0.0:
+            frame = frame
+        else:
+            try:
+                0.0 <= self.noise < 1.0
+            except ValueError:
+                print(f"Value of noise has to be in range [0,1). Got noise={noise}")
+            noisemask = self.np_random.choice(a=[False, True], size=frame.shape, p=[self.noise, 1.0-self.noise])
+            frame = noisemask * frame
+        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        return frame[:, :, None]
+
+class AtariWrapper_NoisyWarp(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
+    """
+    Atari 2600 preprocessings
+
+    Specifically:
+
+    * Noop reset: obtain initial state by taking random number of no-ops on reset.
+    * Frame skipping: 4 by default
+    * Max-pooling: most recent two observations
+    * Termination signal when a life is lost.
+    * Resize to a square image: 84x84 by default
+    * Grayscale observation
+    * Clip reward to {-1, 0, 1}
+    * Sticky actions: disabled by default
+    * Noisy image: some random pixels are blacked out
+
+    See https://danieltakeshi.github.io/2016/11/25/frame-skipping-and-preprocessing-for-deep-q-networks-on-atari-2600-games/
+    for a visual explanation.
+
+    .. warning::
+        Use this wrapper only with Atari v4 without frame skip: ``env_id = "*NoFrameskip-v4"``.
+
+    :param env: Environment to wrap
+    :param noop_max: Max number of no-ops
+    :param frame_skip: Frequency at which the agent experiences the game.
+        This correspond to repeating the action ``frame_skip`` times.
+    :param screen_size: Resize Atari frame
+    :param terminal_on_life_loss: If True, then step() returns done=True whenever a life is lost.
+    :param clip_reward: If True (default), the reward is clip to {-1, 0, 1} depending on its sign.
+    :param action_repeat_probability: Probability of repeating the last action
+    """
+
+    def __init__(
+        self,
+        env: gym.Env,
+        noop_max: int = 30,
+        frame_skip: int = 4,
+        screen_size: int = 84,
+        terminal_on_life_loss: bool = True,
+        clip_reward: bool = True,
+        action_repeat_probability: float = 0.0,
+        noise: float = 0.0
+    ) -> None:
+        if action_repeat_probability > 0.0:
+            env = StickyActionEnv(env, action_repeat_probability)
+        if noop_max > 0:
+            env = NoopResetEnv(env, noop_max=noop_max)
+        # frame_skip=1 is the same as no frame-skip (action repeat)
+        if frame_skip > 1:
+            env = MaxAndSkipEnv(env, skip=frame_skip)
+        if terminal_on_life_loss:
+            env = EpisodicLifeEnv(env)
+        if "FIRE" in env.unwrapped.get_action_meanings():  # type: ignore[attr-defined]
+            env = FireResetEnv(env)
+        env = NoisyWarp(env, width=screen_size, height=screen_size, noise=noise)
+        if clip_reward:
+            env = ClipRewardEnv(env)
+
+        super().__init__(env)
+        
+######################################################################################
 class NoisyFrame(gym.ObservationWrapper[np.ndarray, int, np.ndarray]):
     def __init__(self, env: gym.Env, noise: float) -> None:
         super().__init__(env)
@@ -375,6 +472,7 @@ class AtariWrapper_NoisyFrame(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
             env = ClipRewardEnv(env)
 
         super().__init__(env)
+##########################################################################
 
 def make_atari_env_Custom(
     env_id: Union[str, Callable[..., gym.Env]],
